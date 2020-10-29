8e4b63720a58b4b0903a551b7969db1c3c300009
kirill ginko <kirillginko@gmail.com>
2020-10-29 13:44:22 -0400

    portfolio basics

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
